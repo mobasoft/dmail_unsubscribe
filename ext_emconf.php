890d6988c1176527d04670c105f75548105e12f5
Steffen Scheibe <mail@mobasoft.de>
2017-04-11 21:06:37 +0200

    [TASK] inital commit

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "dmail_unsubscribe".
+ *
+ * Auto generated 19-03-2017 22:15
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Direct mail unsubscription',
+  'description' => 'Allows a direct mail recipient to unsubscribe himself from receiving direct mail newsletters. The recipient might come from fe_users or tt_address table.',
+  'category' => 'plugin',
+  'version' => '0.4.0',
+  'state' => 'beta',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearcacheonload' => 0,
+  'author' => 'Roman Buechler',
+  'author_email' => 'rb@synac.com',
+  'author_company' => 'Synac Technology, S.L.',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
